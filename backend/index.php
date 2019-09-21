d2cfb972f3184102f1771cfc0b06e161dca06933
popoway <31576265+popoway@users.noreply.github.com>
2019-09-21 12:03:33 -0400

    Upload Backend

--- backend/index.php
+++ backend/index.php
@@ -1,25 +1,17 @@
 <?php
-
 // define the question
 $question = $_GET["query"];
+// echo $question;
+
 // load and decode JSON
-$joke_list = json_decode(file_get_contents("joke_list.json"));
+$joke_list = json_decode(file_get_contents("joke_list.json"),true);
 // iterate array to find matching question
-
-echo urlencode("dadj adj");
-echo $joke_list;
-
-// foreach ($joke_list as $qa) {
-//   foreach ($qa as $key => $value) {
-//     if ($key == "Question") {
-//       foreach ($value as $q) {
-//         echo $qa[1];
-//         // echo urlencode($qa[1][0])."\n";
-//         if ($q == urlencode($question)) {
-//           echo $qa[1][rand(0, count($qa[1]) - 1)];
-//         }
-//       }
-//     }
-//   }
-// }
-?>
+foreach ($joke_list as $qa_set) {
+  $qa_set_json = json_encode($qa_set); // convert back to JSON
+  $obj = json_decode($qa_set_json);
+  $q = $obj->{'Question'};
+  $a = $obj->{'Answer'};
+  if ($question == $q){
+    echo $a;
+  }
+}
